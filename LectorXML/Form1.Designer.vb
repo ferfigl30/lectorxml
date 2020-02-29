ada781c4ea7c8b1300f57977d51d4bf32f6bbf34
ferfigl30 <fernando_figueroa_l@hotmail.com>
2020-02-29 08:23:03 -0700

    Add files via upload

---
+++ LectorXML/Form1.Designer.vb
@@ -0,0 +1,56 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLectorXML
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLeerXML = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'btnLeerXML
+        '
+        Me.btnLeerXML.Location = New System.Drawing.Point(59, 52)
+        Me.btnLeerXML.Name = "btnLeerXML"
+        Me.btnLeerXML.Size = New System.Drawing.Size(75, 23)
+        Me.btnLeerXML.TabIndex = 0
+        Me.btnLeerXML.Text = "Leer XML"
+        Me.btnLeerXML.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'frmLectorXML
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(674, 474)
+        Me.Controls.Add(Me.btnLeerXML)
+        Me.Name = "frmLectorXML"
+        Me.Text = "Lector de XML"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnLeerXML As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ LectorXML/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Xml
+
+Public Class frmLectorXML
+
+    Private Sub btnLeerXML_Click(sender As Object, e As EventArgs) Handles btnLeerXML.Click
+        Dim nsCFDi As XNamespace = "http://www.sat.gob.mx/cfd/3"
+
+        Try
+            OpenFileDialog1.Filter = "archivo XML|*.xml"
+            If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                'Dim documentoXML As XDocument = XDocument.Load("C:\Users\sistemas\Documents\FFL\CLIENTES\GACSE\Ejemplo impuesto Trasladado.xml")
+                Dim documentoXML As XDocument = XDocument.Load(OpenFileDialog1.FileName)
+                Dim documentoXML2 As XmlDocument
+                documentoXML2 = New XmlDocument()
+                documentoXML2.Load(OpenFileDialog1.FileName)
+                'documentoXML2.Load("C:\Users\sistemas\Documents\FFL\CLIENTES\GACSE\Ejemplo impuesto Trasladado.xml")
+
+                Dim total As String
+                total = documentoXML2.DocumentElement.GetAttribute("Total").ToString
+                MsgBox("El total es: " + total)
+
+                For Each ldocumento As XElement In documentoXML.Descendants(nsCFDi + "Comprobante").Elements()
+                    Dim tipo As String = ldocumento.Name.LocalName.ToString
+                    'Dim nombre2 As String = ldocumento
+                    If tipo = "Emisor" Or tipo = "Receptor" Then
+                        Dim rfc1 As String = ldocumento.Attribute("Rfc").Value
+                        MsgBox("RFC " + tipo + ": " + rfc1)
+                    End If
+
+                Next
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.ToString())
+        End Try
+    End Sub
+End Class
